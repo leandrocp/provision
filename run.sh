b49a9dc849583a8890c55a089377ea7efc18e3cb
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2013-11-01 19:38:01 -0200

    bootstrap

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-#sudo apt-get install python-software-properties -y
-#sudo add-apt-repository ppa:rquillo/ansible -y
-#sudo apt-get update
-#sudo apt-get install ansible -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git python-software-properties -y
+sudo add-apt-repository ppa:rquillo/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
+
+cd ~
+git clone https://github.com/leandrocp/provision.git
+cd provision
+
+sudo apt-get purge git -y
+sudo apt-get autoremove -y 
 
 ansible-playbook -c local -i hosts vm.yml
